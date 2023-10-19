a090af36e5d6ecdea59bf0c59b4845d2b06ac1ad
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-10-19 16:49:57 +0300

    almost done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import ButtonsComponent from "./ButtonsComponent";
+// import AlertsComponent from "./AlertsComponent";
+// import AccordionsComponent from "./AccordionsComponent";
+// import BadgeComponent from './BadgeComponent'
+import NavbarComponent from './NavbarComponent'
+
+function App() {
+  return (
+    <>
+      {/* <ButtonsComponent />
+      <AlertsComponent />
+      <AccordionsComponent />
+      <BadgeComponent /> */}
+      <NavbarComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/ButtonsComponent.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import { Button } from 'react-bootstrap';
+
+function ButtonsComponent() {
+  return (
+    <>
+    <h2>Buttons Component</h2>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </>
+  );
+}
+
+export default ButtonsComponent;
